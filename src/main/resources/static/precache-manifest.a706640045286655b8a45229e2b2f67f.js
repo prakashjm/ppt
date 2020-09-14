819844c109d0443a0f3f301ebf66d453fd812f79
prakash <prakjm@gmail.com>
2020-09-14 19:58:04 +0400

    dd

---
+++ src/main/resources/static/precache-manifest.a706640045286655b8a45229e2b2f67f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d713765ed1997f499a4bd435f511b596",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58704c99390bb5ef18fa",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6c904517c7ec1c6a260c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58704c99390bb5ef18fa",
+    "url": "/static/js/2.7ded75db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7ded75db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c904517c7ec1c6a260c",
+    "url": "/static/js/main.92d65c63.chunk.js"
+  },
+  {
+    "revision": "56daa67b9be550083164",
+    "url": "/static/js/runtime-main.25794b14.js"
+  }
+]);
