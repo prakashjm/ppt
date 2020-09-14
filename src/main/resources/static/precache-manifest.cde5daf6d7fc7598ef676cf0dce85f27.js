e231be4403d2659e44dfff2df244693fda5e9c00
prakash <prakjm@gmail.com>
2020-09-14 19:44:25 +0400

    first commi1st

---
+++ src/main/resources/static/precache-manifest.cde5daf6d7fc7598ef676cf0dce85f27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387b0c7343b19559304ff725bf1ccc4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6a80ee6fb44b3a048442",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "6a80ee6fb44b3a048442",
+    "url": "/static/js/main.212028cf.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
